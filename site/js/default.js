671383a32f4da2138ea584741375a3e3e60e47e7
cofire <yli_666@163.com>
2017-06-27 17:13:55 +0800

    cofire

--- site/js/default.js
+++ site/js/default.js
@@ -1,17 +1,14 @@
 $(function(){ 
-    $("#btn").click(function(){ 
-        $.getJSON("js/userinfo.json",function(data){ 
-            var $jsontip = $("#jsonTip"); 
-            var strHtml = "123";//存储数据的变量 
-            $jsontip.empty();//清空内容 
-            $.each(data,function(infoIndex,info){ 
-                strHtml += "姓名："+info["name"]+"<br>"; 
-                strHtml += "性别："+info["sex"]+"<br>"; 
-                strHtml += "邮箱："+info["email"]+"<br>"; 
-                strHtml += "<hr>" 
-            }) 
-            $jsontip.html(strHtml);//显示处理后的数据 
+    $.getJSON("js/userinfo.json",function(data){ 
+        var $jsontip = $("#jsonTip"); 
+        var strHtml = "123";//存储数据的变量 
+        $jsontip.empty();//清空内容 
+        $.each(data,function(infoIndex,info){ 
+            strHtml += "姓名："+info["name"]+"<br>"; 
+            strHtml += "性别："+info["sex"]+"<br>"; 
+            strHtml += "邮箱："+info["email"]+"<br>"; 
+            strHtml += "<hr>" 
         }) 
+        $jsontip.html(strHtml);//显示处理后的数据 
     }) 
-
 })
