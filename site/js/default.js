ca6c79aad6568991f334cd27d737eb726df1867e
cofire <yli_666@163.com>
2017-06-27 17:48:06 +0800

    cofire

--- site/js/default.js
+++ site/js/default.js
@@ -1,11 +1,9 @@
 $(function(){ 
     $.getJSON("urlJson/site.json",function(data){ 
         var $jsontip = $("#jsonTip"); 
-        var strHtml = "123";//存储数据的变量 
+        var strHtml = "";//存储数据的变量 
         $jsontip.empty();//清空内容 
         $.each(data,function(infoIndex,info){ 
-            strHtml += "标题："+info["titile"]+"<br>"; 
-            strHtml += "网址："+info["url"]+"<br>"; 
             strHtml += "<hr>" 
         }) 
         $jsontip.html(strHtml);//显示处理后的数据 
