2945f7785f32b1b70f9cf33b5e6ff9aefbcc87b2
cofire <yli_666@163.com>
2017-06-27 17:44:04 +0800

    cofire

--- site/js/default.js
+++ site/js/default.js
@@ -3,8 +3,9 @@ $(function(){
         var $jsontip = $("#jsonTip"); 
         var strHtml = "123";//存储数据的变量 
         $jsontip.empty();//清空内容 
-        $.each(data,function(infoIndex,info){
-            strHtml += "<a href='"+info["url"]+"'>"++info["titile"]+"</a>";
+        $.each(data,function(infoIndex,info){ 
+            strHtml += "标题："+info["titile"]+"<br>"; 
+            strHtml += "网址："+info["url"]+"<br>"; 
             strHtml += "<hr>" 
         }) 
         $jsontip.html(strHtml);//显示处理后的数据 
