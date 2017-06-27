4a56297da339b35c38f6ee813e1c53c5cba8bf91
cofire <yli_666@163.com>
2017-06-27 17:49:51 +0800

    cofire

--- site/js/default.js
+++ site/js/default.js
@@ -4,6 +4,7 @@ $(function(){
         var strHtml = "";//存储数据的变量 
         $jsontip.empty();//清空内容 
         $.each(data,function(infoIndex,info){ 
+            strHtml += "<a href='www.baidu.com'>百度"+"</a>"
             strHtml += "<hr>" 
         }) 
         $jsontip.html(strHtml);//显示处理后的数据 
